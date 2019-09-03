89da4177a6fea41b07a9560c86250da4fb3fdf08
Jean Luca Colombo <jeanlucacolombo@outlook.com>
2019-09-02 21:13:37 -0300

    Created IPhasor interface.

---
+++ Source/IPhasor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace Pamola.Phasor
+{
+    interface IPhasor
+    {
+        Complex Value { get; }
+
+        double RmsValue { get; }
+
+        double Period { set; get; }
+    }
+}
